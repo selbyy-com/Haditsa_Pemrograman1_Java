cf4ac48563d7b1b72925f1dd471204882681bc22
selbyy-com <arshabiyah@gmail.com>
2026-04-06 07:17:38 +0700

    Add files via upload

--- selbylatihan2.java
+++ selbylatihan2.java
@@ -1,13 +1,29 @@
-package Pertemuan2;
-
 import java.util.Scanner;
-
 public class selbylatihan2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String nama; int usia;
-        System.out.print("Input nama: ");
-        nama = input.nextLine();
-        System.out.print("nama "+nama);
-    }
+
+        System.out.println("Nilai Mahasiswa");
+        System.out.println("=============================================");
+        System.out.print("Masukan nilai presensi : ");
+        float presensi = input.nextFloat();
+
+        System.out.print("Masukan nilai tugas : ");
+        float tugas = input.nextFloat();
+        
+        System.out.print("masukan nilai UTS : ");
+        float uts = input.nextFloat();
+
+        System.out.print("Masukan nilai UAS : ");
+        float uas = input.nextFloat();
+
+        float totalPresensi = presensi / 21 * 10;
+        float totalTugas = tugas * 21 / 100;
+        float totalUTS = uts * 30 / 100;
+        float totalUAS = uas * 40 / 100;
+
+        double total = totalPresensi + totalTugas + totalUTS + totalUAS;
+        System.out.println("=============================================");
+        System.out.printf("total : %.2f ", total);
+    }    
 }
