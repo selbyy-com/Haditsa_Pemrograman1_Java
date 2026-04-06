25185db28409c7717fb809d090cc3ac2553bf417
selbyy-com <arshabiyah@gmail.com>
2026-04-06 07:15:51 +0700

    Add files via upload

---
+++ selbylatihan2.java
@@ -0,0 +1,13 @@
+package Pertemuan2;
+
+import java.util.Scanner;
+
+public class selbylatihan2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String nama; int usia;
+        System.out.print("Input nama: ");
+        nama = input.nextLine();
+        System.out.print("nama "+nama);
+    }
+}
